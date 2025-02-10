18598e33b42f5ba8fc1c03118417d1ac111d92b2
Guilherme <greinguilherme@gmail.com>
2025-02-10 17:55:42 -0300

    Primeiro commit

---
+++ src/main/java/com/guilherme/dsdlist/DsdlistApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.dsdlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/dsdlist/DsdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.dsdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
